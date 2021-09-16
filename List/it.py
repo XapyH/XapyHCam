74639c9d9e04d9ea9374b70cc6b6827980edc519
Харун <77844554+XapyH@users.noreply.github.com>
2021-09-16 22:26:17 +0300

    Update it.py

--- List/it.py
+++ List/it.py
@@ -16,7 +16,7 @@ headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleW
 
 
 
-def italy():
+def Italya():
     global page
     res = requests.get('https://www.insecam.org/en/bycountry/IT/', headers=headers)
     findpage = re.findall('"?page=",\s\d+', res.text)[1]
